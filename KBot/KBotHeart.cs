2a2198675e31c58d7f07478bd562f35a377cc598
Scarlett <Scarlett@kayde.me>
2024-05-23 12:14:44 +0100

    basics of a discord bot running as a hosted service.

---
+++ KBot/KBotHeart.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Addons.Hosting;
+using Discord.Addons.Hosting.Util;
+using Discord.WebSocket;
+
+namespace KBot;
+
+public class KBotHeart(DiscordSocketClient client, ILogger<DiscordClientService> logger)
+    : DiscordClientService(client, logger)
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Client.WaitForReadyAsync(stoppingToken);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Logger.LogInformation("Still here at {time}", DateTimeOffset.UtcNow);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
--- KBot/Program.cs
+++ KBot/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using KBot;
 
-Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDiscordHost((config, _) =>
+{
+    config.SocketConfig = new DiscordSocketConfig
+    {
+        LogLevel = LogSeverity.Verbose,
+        AlwaysDownloadUsers = true, 
+        MessageCacheSize = 200,
+        GatewayIntents = GatewayIntents.All
+    };
+
+    config.Token = builder.Configuration["token"] ?? string.Empty;
+});
+
+builder.Services.AddCommandService((config, _) =>
+{
+    config.DefaultRunMode = RunMode.Async;
+    config.CaseSensitiveCommands = false;
+});
+
+builder.Services.AddInteractionService((config, _) =>
+{
+    config.LogLevel = LogSeverity.Info;
+    config.UseCompiledLambda = true;
+});
+
+
+
+
+builder.Services.AddHostedService<KBotHeart>();
+
+var host = builder.Build();
+await host.RunAsync();
